3cde946298efad3c25ab917edcd5fa13453997e3
ohko <ohko@qq.com>
2019-08-22 10:39:50 +0800

    美化界面

--- server.js
+++ server.js
@@ -9,18 +9,10 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", express.static("./public"))
 
-app.get("/", async (req, res) => {
-  res.send(`
-<form method="POST" action="/run" target="_blank">
-<textarea name="task" style="width:500px;height:300px;">${JSON.stringify(await taskSimple(), true, 2)}</textarea>
-<input type="submit" value="SIMPLE">
-</form>
-<form method="POST" action="/run" target="_blank">
-<textarea name="task" style="width:500px;height:300px;">${JSON.stringify(await taskFull(), true, 2)}</textarea>
-<input type="submit" value="FULL">
-</form>
-  `)
+app.get("/demo.json", async (req, res) => {
+  res.send({ "simple": JSON.stringify(await taskSimple(), true, 2), "full": JSON.stringify(await taskFull(), true, 2) })
 });
 
 app.post("/run", async (req, res) => {
