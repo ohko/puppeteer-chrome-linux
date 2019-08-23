f40789cd8e517021d912db2946c07f9637414ec2
ohko <ohko@qq.com>
2019-08-23 16:03:23 +0800

    增加日志内容返回

--- runner.js
+++ runner.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const axios = require("axios")
 
 const runner = async (task) => {
-   let browser
+   let browser, logs = []
    let defaultTask = {
       "ws": "",
       "browserArgs": {
@@ -20,13 +20,14 @@ const runner = async (task) => {
    }
    task = Object.assign(defaultTask, task)
 
-   if (task.action.url == "") return { no: 1, data: "url is empty" }
-   if (task.action.steps.length == 0) return { no: 1, data: "not found steps" }
+   if (task.action.url == "") return { no: 1, data: "url is empty", logs: logs }
+   if (task.action.steps.length == 0) return { no: 1, data: "not found steps", logs: logs }
 
    try {
       // init
       browser = task.ws ? await puppeteer.connect({ browserWSEndpoint: task.ws }) : await puppeteer.launch(task.browserArgs);
-      const page = task.ws ? (await browser.pages()).shift() : await browser.newPage();
+      const ps = await browser.pages()
+      const page = ps.length ? ps.shift() : await browser.newPage();
 
       // setting
       await page.setViewport(task.viewport)
@@ -34,12 +35,15 @@ const runner = async (task) => {
       task.userAgent && await page.setUserAgent(task.userAgent)
 
       // steps
+      console.log("===== START:", task.action.url, "=====")
+      logs.push("START: " + task.action.url)
       await page.goto(task.action.url, task.action.options);
 
       let result
       for (let i = 0; i < task.action.steps.length; i++) {
          const step = task.action.steps[i];
-         console.log("===== STEP:", (i + 1), "=====\n", step)
+         console.log("----- STEP:", (i + 1), "-----\n" + JSON.stringify(step, "", 2))
+         logs.push("STEP." + (i + 1) + ": " + JSON.stringify(step))
 
          step.waitText && await page.waitForFunction(step => {
             try { return document.documentElement.outerHTML.match(new RegExp(step.waitText)) } catch (e) { return null }
@@ -48,9 +52,9 @@ const runner = async (task) => {
 
          result = step.js ? await page.evaluate(step.js) : await page.content()
       }
-      return { no: 0, data: result }
+      return { no: 0, data: result, logs: logs }
    } catch (e) {
-      return { no: 1, data: e.message ? e.message : e }
+      return { no: 1, data: e.message ? e.message : e, logs: logs }
    } finally {
       if (browser) {
          // close
@@ -65,6 +69,7 @@ const taskFull = async () => {
    return {
       "ws": ws,
       "browserArgs": {
+         "slowMo": 100,
          "headless": false,
          "args": ["--no-sandbox", "--proxy-server-(remove this)=socks5://127.0.0.1:1080"],
       },
@@ -72,18 +77,17 @@ const taskFull = async () => {
       "timeout": 30000,
       "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
       "action": {
-         "url": "https://www.baidu.com",
-         "options": { "timeout": 30000, "waitUntil": "load", "referer": "" },
+         "url": "https://bing.com",
+         "options": { "waitUntil": "domcontentloaded", "referer": "" },
          "steps": [
             {
-               "timeout": 30000,
-               "waitSelector": "#kw",
-               "js": "document.getElementById(\"kw\").value=\"baidu\";document.getElementById(\"su\").click()"
+               "waitSelector": ".b_searchbox",
+               "js": "document.querySelector('.b_searchbox').value='apple stock price';document.querySelector('.b_searchbox').form.submit()"
             },
             {
-               "timeout": 30000,
-               "waitText": "全球最大的中文搜索引擎",
-               "js": "(_=>{let hrefs=[];let a=document.getElementsByTagName(\"a\");for(var i=0;i<a.length;i++) hrefs.push(a[i].href);return hrefs;})()"
+               "waitText": "NASDAQ: AAPL",
+               "waitSelector": ".b_focusTextMedium",
+               "js": "(_=>{return document.querySelector('.b_focusTextMedium').textContent})()"
             }
          ]
       }
@@ -94,15 +98,16 @@ const taskSimple = async () => {
    return {
       "browserArgs": { "args": ["--no-sandbox"] },
       "action": {
-         "url": "https://www.baidu.com",
+         "url": "https://bing.com",
          "steps": [
             {
-               "waitSelector": "#kw",
-               "js": "document.getElementById(\"kw\").value=\"baidu\";document.getElementById(\"su\").click()"
+               "waitSelector": ".b_searchbox",
+               "js": "document.querySelector('.b_searchbox').value='apple stock price';document.querySelector('.b_searchbox').form.submit()"
             },
             {
-               "waitText": "全球最大的中文搜索引擎",
-               "js": "(_=>{let hrefs=[];let a=document.getElementsByTagName(\"a\");for(var i=0;i<a.length;i++) hrefs.push(a[i].href);return hrefs;})()"
+               "waitText": "NASDAQ: AAPL",
+               "waitSelector": ".b_focusTextMedium",
+               "js": "(_=>{return document.querySelector('.b_focusTextMedium').textContent})()"
             }
          ]
       }
--- test.js
+++ test.js
@@ -5,7 +5,8 @@ const { runner, taskFull, taskSimple } = require("./runner");
 (async () => {
    const result = await runner(await taskSimple());
    // const result = await runner(await taskFull());
-   console.log("RESULT:", result.no == 0 ? result.data.length : result.data)
+   console.log("RESULT:", result.data)
+   console.log("LOGS:\n", result.logs.join("\n"))
    process.exit()
 })();
 
