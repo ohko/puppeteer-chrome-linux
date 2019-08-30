d9fd22040eadacf64eb49c62763ec5463e9d63a6
ohko <ohko@qq.com>
2019-08-30 14:24:34 +0800

    ws, browser, page 预申请，外部释放，防止忘记close

--- runner.js
+++ runner.js
@@ -4,7 +4,18 @@ const axios = require("axios")
 const runner = async (script) => {
    let logs = []
    try {
+      let ws, browser, page;
+      // 查找已有的ws
+      try { ws = (await axios.get('http://127.0.0.1:9222/json/version')).data.webSocketDebuggerUrl } catch (e) { }
       const result = await eval(`(async _=>{` + script.replace(/\\/g, "\\\\") + `})()`)
+      try {
+         page && page.close();
+         if (!ws) {
+            browser && browser.close();
+         }
+      } catch (e) {
+         console.log(e)
+      }
       return { no: 0, data: result, logs: logs }
    } catch (e) {
       return { no: 1, data: e.message ? e.message : e, logs: logs }
@@ -14,12 +25,8 @@ const runner = async (script) => {
 const taskFull = async () => {
    return `// 复杂案例: 查询baidu的股价
 
-// 查找已有的ws
-let ws
-try { ws = (await axios.get('http://127.0.0.1:9222/json/version')).data.webSocketDebuggerUrl } catch (e) { }
-
 // 初始化
-const browser = ws ?
+browser = ws ?
    await puppeteer.connect({ browserWSEndpoint: ws }) :
    await puppeteer.launch({
       headless:false,
@@ -27,61 +34,63 @@ const browser = ws ?
       args:["--no-sandbox", "--proxy-server-(remove this)=socks5://127.0.0.1:1080"]
    });
 const ps = await browser.pages()
-const page = ps.length ? ps.shift() : await browser.newPage();
+page = ps.length ? ps.shift() : await browser.newPage();
 await page.goto("about:blank")
+let rs = "";
 
-// 设置
-await page.setDefaultTimeout(30000)
-await page.setDefaultNavigationTimeout(30000)
-await page.setViewport({width:1024, height:768})
-await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
+try{
+   // 设置
+   await page.setDefaultTimeout(30000)
+   await page.setDefaultNavigationTimeout(30000)
+   await page.setViewport({width:1024, height:768})
+   await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
 
-// console
-page.on('console', msg => {
-   for (let i = 0; i < msg.args().length; ++i) console.log(i, msg.args()[i]);
-});
+   // console
+   page.on('console', msg => {
+      // for (let i = 0; i < msg.args().length; ++i) console.log(i, msg.args()[i]);
+   });
 
-// 对话框
-page.on('dialog', async dialog => {
-   console.log(dialog.message());
-   await dialog.dismiss();
-});
+   // 对话框
+   page.on('dialog', async dialog => {
+      console.log(dialog.message());
+      await dialog.dismiss();
+   });
 
-logs.push("START: https://www.baidu.com")
-// 打开baidu
-await page.goto("https://www.baidu.com")
+   logs.push("START: https://www.baidu.com")
+   // 打开baidu
+   await page.goto("https://www.baidu.com")
 
-logs.push("STEP1: 搜索'百度股价'")
-// 等待搜索输入框
-await page.waitForSelector("#kw")
-// 输入搜索词，点击搜索
-await page.evaluate("var k=document.querySelector('#kw');k.value='百度股价';k.form.submit()")
+   logs.push("STEP1: 搜索'百度股价'")
+   // 等待搜索输入框
+   await page.waitForSelector("#kw")
+   // 输入搜索词，点击搜索
+   await page.evaluate("var k=document.querySelector('#kw');k.value='百度股价';k.form.submit()")
 
-logs.push("STEP2: 下一页")
-// 下一页
-await page.waitForSelector("#content_left")
-// 输入搜索词，点击搜索
-await page.evaluate("(_=>{document.querySelector('#content_left').remove();document.querySelector('.n').click(); return document.documentElement.outerHTML.match(/上一页/)?0:1})()")
+   logs.push("STEP2: 下一页")
+   // 下一页
+   await page.waitForSelector("#content_left")
+   // 输入搜索词，点击搜索
+   await page.evaluate("(_=>{document.querySelector('#content_left').remove();document.querySelector('.n').click(); return document.documentElement.outerHTML.match(/上一页/)?0:1})()")
 
-logs.push("STEP3: 上一页")
-// 上一页
-await page.waitForSelector("#content_left")
-// 输入搜索词，点击搜索
-await page.evaluate("(_=>{document.querySelector('#content_left').remove();document.querySelector('.n').click(); return document.documentElement.outerHTML.match(/上一页/)?0:1})()")
+   logs.push("STEP3: 上一页")
+   // 上一页
+   await page.waitForSelector("#content_left")
+   // 输入搜索词，点击搜索
+   await page.evaluate("(_=>{document.querySelector('#content_left').remove();document.querySelector('.n').click(); return document.documentElement.outerHTML.match(/上一页/)?0:1})()")
 
-logs.push("STEP4: 查找股价")
-// 等待股价关键词
-await page.waitForSelector(".op-stockdynamic-moretab-cur-num")
-// 返回股价信息
-const rs = await page.evaluate("(_=>{return document.querySelector('.op-stockdynamic-moretab-cur-num').textContent})()")
+   logs.push("STEP4: 查找股价")
+   // 等待股价关键词
+   await page.waitForSelector(".op-stockdynamic-moretab-cur-num")
+   // 返回股价信息
+   const rs = await page.evaluate("(_=>{return document.querySelector('.op-stockdynamic-moretab-cur-num').textContent})()")
 
-logs.push("STEP5: 股价 " + rs)
-// 如果是链接已有的ws，就不关闭
-ws || await page.close()
-ws || await browser.close()
-
-// 返回
-return rs
+   logs.push("STEP5: 股价 " + rs)
+}catch(e){
+   logs.push(e.Message ? e.Message : e)
+}finally{   
+   // 返回
+   return rs
+}
 `
 }
 
@@ -89,35 +98,37 @@ const taskSimple = async () => {
    return `// 简单案例: 查询apple的股价
 
 // 初始化
-const browser = await puppeteer.launch({args:["--no-sandbox"]});
-const page = await browser.newPage();
+browser = await puppeteer.launch({args:["--no-sandbox"]});
+page = await browser.newPage();
+let rs = "";
 
-logs.push("START: https://www.baidu.com")
-// 打开baidu
-await page.goto("https://www.baidu.com", {waitUntil:"domcontentloaded"})
+try{
+   logs.push("START: https://www.baidu.com")
+   // 打开baidu
+   await page.goto("https://www.baidu.com", {waitUntil:"domcontentloaded"})
 
-logs.push("STEP1: 搜索'apple股票'")
-// 等待搜索输入框
-await page.waitForSelector("#kw")
-// 输入搜索词，点击搜索
-await page.evaluate("const x=document.querySelector('#kw');x.value='apple股票';x.form.submit()")
+   logs.push("STEP1: 搜索'apple股票'")
+   // 等待搜索输入框
+   await page.waitForSelector("#kw")
+   // 输入搜索词，点击搜索
+   await page.evaluate("const x=document.querySelector('#kw');x.value='apple股票';x.form.submit()")
 
-logs.push("STEP2: 查找'AAPL'")
-// 等待股价关键词
-await page.waitFor(_ => {
-   try {return !!document.documentElement.outerHTML.match(/AAPL/)} 
-   catch (e) { return false }
-})
-// 返回股价信息
-const rs = await page.evaluate("(_=>{return document.querySelector('.op-stockdynamic-moretab-cur-num').textContent})()")
+   logs.push("STEP2: 查找'AAPL'")
+   // 等待股价关键词
+   await page.waitFor(_ => {
+      try {return !!document.documentElement.outerHTML.match(/AAPL/)} 
+      catch (e) { return false }
+   })
+   // 返回股价信息
+   const rs = await page.evaluate("(_=>{return document.querySelector('.op-stockdynamic-moretab-cur-num').textContent})()")
 
-logs.push("STEP3: 股价 " + rs)
-// 关闭
-await page.close()
-await browser.close()
-
-// 返回
-return rs
+   logs.push("STEP3: 股价 " + rs)
+}catch(e){
+   logs.push(e.Message ? e.Message : e)
+}finally{   
+   // 返回
+   return rs
+}
 `}
 
 module.exports = { runner, taskFull, taskSimple }
